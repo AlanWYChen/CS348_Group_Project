979151741c0f1052ff12f57a7610b66c88b9842b
AlanWYChen <alan.ch3n@gmail.com>
2024-06-25 20:28:20 -0400

    added observer + model

---
+++ myMovies/src/Application/Common/Models/Model.ts
@@ -0,0 +1,21 @@
+import { Subject } from "../../../Common/Utility/observer";
+
+
+export class Model extends Subject {
+    
+    constructor() {
+
+        super();
+
+        // pull data from dataabase
+
+        
+
+    }
+
+    update(): void {
+
+    }
+
+
+}
