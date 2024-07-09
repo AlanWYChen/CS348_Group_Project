9b973f19e0e42309ed30642db754185f91288c57
Aumio Islam <a38islam@uwaterloo.ca>
2024-07-09 15:16:38 -0400

    Fixed endpoint code to use core sqlalchemy

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@/film_folio_sample'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@/sample_film_folio'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
-from backend import models, routes
+
+from backend import routes 
--- backend/routes.py
+++ backend/routes.py
@@ -1,24 +1,48 @@
 from flask import request, Request, Response, jsonify
 
-from backend import app, db
+from backend import app, db, engine
 
-from backend.models.movie import Movie
+from .db_functions import *
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>API Backend</p>"
 
 @app.route("/movie", methods=['GET'])
 def get_movie_from_id():
-    if request.method == 'GET':
-        id = request.get_json()['id']
+    print('here')
+    id = request.get_json()['id']
+    print(id)
 
-        if id:
-            movie = Movie.query.get(id)
-        else:
-            return Response(status=204)
+    if id:
+        print(id)
+        print(engine)
+        movie = db_get_movie_by_id(engine, id)
+    else:
+        return Response(status=204)
 
-        if movie:
-            return jsonify(movie)
+    if movie:
+        return jsonify(movie)
+
+    return Response(status=204)
+
+@app.route("/all_movies", methods=["GET"])
+def get_all_movies():
+    all_movies = db_get_all_movies(engine)
+    print(all_movies)
+    if all_movies: 
+        return jsonify(all_movies)
+    return Response(status=204)
+
+@app.route("/movies_in_list", methods=["GET"])
+def get_movies_by_list_id():
+    list_id = request.get_json()['list_id']
+    if list_id:
+        movies = db_get_movies_in_list(engine, list_id)
+    else:
+        return Response(status=204)
+
+    if movies:
+        return jsonify(movies)
 
     return Response(status=204)
