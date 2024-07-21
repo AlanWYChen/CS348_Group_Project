9e5d012e98225364e6de0dd97cfdc69f12fda8ee
shriya-31 <kaisthas31@gmail.com>
2024-07-21 01:24:12 -0400

    1.Custom User list creation + deletion endpoints 2.Getting all custom lists for a given user

--- backend/db_functions.py
+++ backend/db_functions.py
@@ -1,6 +1,10 @@
 import os, re
 from .db_connect import run_query
 
+def db_get_all_movies(engine): 
+    all_movies = run_query(engine, "SELECT title, id FROM movies ORDER BY title;", True)
+    return all_movies
+
 def db_get_movie_by_id(engine, movie_id): 
     movie_info = run_query(engine, "SELECT * from movies WHERE id=%s;" % (movie_id), True)
     return movie_info
@@ -17,7 +21,14 @@ def db_get_movies_in_list(engine, list_id):
                                  ;''' % (list_id), True)
     return movies_in_list
 
-# get all the movies from the DB and display in home page
-def db_get_all_movies(engine): 
-    all_movies = run_query(engine, "SELECT title, id FROM movies ORDER BY title;", True)
-    return all_movies
+
+# get the names & ids of all of a given user's lists 
+def db_get_all_lists_user(engine, user_id): 
+    user_lists = run_query(engine, f'SELECT id, list_name FROM lists WHERE user_id={user_id};', True)
+    return user_lists
+
+def db_add_list(engine, user_id, list_name): 
+    run_query(engine, f"INSERT INTO lists(user_id, list_name) VALUES({user_id}, '{list_name}');", False)
+
+def db_remove_list(engine, list_id): 
+    run_query(engine, f"DELETE FROM lists WHERE id={list_id};", False)
--- backend/routes.py
+++ backend/routes.py
@@ -2,9 +2,8 @@ from flask import request, Request, Response, jsonify
 from backend import app, db, engine
 
 from .db_functions import *
-
-
-
+#TODO: Write all get request woth query params 
+# get all the movies from the DB and display in home page
 @app.route("/")
 def hello_world():
     return "<p>API Backend</p>"
@@ -45,3 +44,61 @@ def get_movies_by_list_id():
         return jsonify(movies)
 
     return Response(status=204)
+
+@app.route("/user_lists", methods=["GET"])
+def get_user_lists():
+    try: 
+        user_id = request.get_json()['user_id']
+    except: 
+        retval = jsonify({
+            'message': 'Bad Request: Missing user id',
+        })
+        return retval
+
+    user_lists = db_get_all_lists_user(engine, user_id)
+
+    if user_lists: 
+        return jsonify(user_lists)
+
+    return Response(status=204)
+
+@app.route("/user_lists", methods=["POST"])
+def add_user_list(): 
+    try: 
+        data = request.get_json()
+        print(data)
+        user_id = data['user_id']
+        list_name = data['list_name']
+        print(user_id, list_name)
+    except: 
+        retval = jsonify({
+            'message': 'Bad Request: Missing user id/list_name',
+        })
+        return retval
+
+    try: 
+        db_add_list(engine, user_id, list_name)
+        return jsonify({
+            'message': f"Successfully created list: {list_name}"
+            }), 201
+    except:
+        retval = jsonify({
+            'message': 'DB Error: The INSERT failed',
+        })
+        return retval, 500
+
+@app.route("/user_lists", methods=["DELETE"])
+def remove_user_list(): 
+    try: 
+        list_id = request.args["list_id"]
+        print(list_id)
+    except: 
+        retval = jsonify({
+            'message': 'Bad Request: Missing list id',
+        })
+        return retval
+
+    db_remove_list(engine, list_id)
+    return jsonify({
+        'message': f"Successfully deleted list: {list_id}"
+    }), 200
