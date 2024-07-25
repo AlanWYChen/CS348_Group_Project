d5fefe16dc3b973868e519d544e3d543d06762aa
Mitchell <mitchelldolny@gmail.com>
2024-07-25 16:21:01 -0400

    changed to prod

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@/sample_film_folio'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@/production_film_folio'
 db = SQLAlchemy(app)
 CORS(app)
 migrate = Migrate(app, db)
